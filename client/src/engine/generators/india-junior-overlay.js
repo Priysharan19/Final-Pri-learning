f9cd095f7fbb009c1954c7e7225486ac8d9dde37
Priysharan19 <Ptri0068@uni.sydney.edu.au>
2026-08-31 11:39:50 +1000

    Register Class 9 NCERT mastery generators

--- client/src/engine/generators/india-junior-overlay.js
+++ client/src/engine/generators/india-junior-overlay.js
@@ -2,10 +2,12 @@ import { indiaJunior as baseIndiaJunior } from './india-junior.js';
 import { NCERT_CLASS8_RATIONAL_GENERATORS } from '../ncert/class8-rational-production.js';
 import { NCERT_CLASS8_LINEAR_GENERATORS } from '../ncert/class8-linear-production.js';
 import { NCERT_CLASS8_3_13_GENERATORS } from '../ncert/class8-chapters-3-13-production.js';
+import { NCERT_CLASS9_GENERATORS } from '../ncert/class9-chapters-production.js';
 
 export const indiaJunior = Object.freeze({
   ...baseIndiaJunior,
   ...NCERT_CLASS8_RATIONAL_GENERATORS,
   ...NCERT_CLASS8_LINEAR_GENERATORS,
-  ...NCERT_CLASS8_3_13_GENERATORS
+  ...NCERT_CLASS8_3_13_GENERATORS,
+  ...NCERT_CLASS9_GENERATORS
 });
