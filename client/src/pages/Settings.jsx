a9417d1b2c229002f1c990f428a899787c206770
Priysharan19 <Ptri0068@uni.sydney.edu.au>
2026-09-02 23:07:13 +1000

    feat(person2): surface classrooms in Settings

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SettingsLegacy from './SettingsLegacy.jsx';
 import CloudAccountPanel from '../components/CloudAccountPanel.jsx';
+import ClassroomPanel from '../components/ClassroomPanel.jsx';
 
 export default function Settings() {
   return (
     <>
       <SettingsLegacy />
       <CloudAccountPanel />
+      <ClassroomPanel />
     </>
   );
 }
