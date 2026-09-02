06b81ffdedbbb7e39828f2e9fa77f0b1c5f032e2
Priysharan19 <Ptri0068@uni.sydney.edu.au>
2026-09-02 23:24:02 +1000

    feat(person2): surface assignments and staff operations in Settings

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import SettingsLegacy from './SettingsLegacy.jsx';
 import CloudAccountPanel from '../components/CloudAccountPanel.jsx';
 import ClassroomPanel from '../components/ClassroomPanel.jsx';
+import AssignmentInboxPanel from '../components/AssignmentInboxPanel.jsx';
+import StaffOperationsPanel from '../components/StaffOperationsPanel.jsx';
 
 export default function Settings() {
   return (
     <>
       <SettingsLegacy />
       <CloudAccountPanel />
+      <AssignmentInboxPanel />
       <ClassroomPanel />
+      <StaffOperationsPanel />
     </>
   );
 }
