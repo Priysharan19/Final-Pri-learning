6ab6fe5c02ebbe095004371e741893b13c574484
Priysharan19 <Ptri0068@uni.sydney.edu.au>
2026-09-06 01:55:04 +1000

    feat(pilot): surface anonymous evidence export in settings

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -4,11 +4,13 @@ import CloudAccountPanel from '../components/CloudAccountPanel.jsx';
 import ClassroomPanel from '../components/ClassroomPanel.jsx';
 import AssignmentInboxPanel from '../components/AssignmentInboxPanel.jsx';
 import StaffOperationsPanel from '../components/StaffOperationsPanel.jsx';
+import PilotEvidencePanel from '../components/PilotEvidencePanel.jsx';
 
 export default function Settings() {
   return (
     <>
       <SettingsLegacy />
+      <PilotEvidencePanel />
       <CloudAccountPanel />
       <AssignmentInboxPanel />
       <ClassroomPanel />
